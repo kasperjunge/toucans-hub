13c8e373b245ac8ac49e4697289b69beab26ab0b
Kasper Junge <kasperjunge@MacBook-Pro-tilhrende-Kasper.local>
2023-12-04 07:59:47 +0100

    to the bone

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 Prompt hub for the [toucans package](https://github.com/kasperjunge/toucans)
 
 ## Backlog
-- Make it possible to query a specific version from the hub with the .from_hub method
-- Enable users to add tags to their prompt functions to mange the versions.
+- Versioning: tags + logic
+- Users: add users + token
--- main.py
+++ main.py
@@ -27,9 +27,8 @@ async def create_prompt(
     return new_prompt
 
 
-@app.get("/prompt-functions/{user_name}/{function_name}", response_model=PromptFunction)
+@app.get("/prompt-functions/{function_name}", response_model=PromptFunction)
 async def read_prompt(
-    username: str,
     function_name: str,
     session: Session = Depends(get_session),
 ):
@@ -37,7 +36,7 @@ async def read_prompt(
     results = session.exec(statement)
     prompt = results.first()
     if not prompt:
-        raise HTTPException(status_code=404, detail="Prompt not found")
+        raise HTTPException(status_code=404, detail="Prompt function not found")
     return prompt
 
 
