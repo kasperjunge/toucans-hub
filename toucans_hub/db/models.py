f7d867ea1977ce3f9031a7969a213bfecf5ed05b
Kasper Junge <kasperjunge@MBPtilhdeKasper.lan>
2023-12-09 15:53:39 +0100

    changed chat_api_config to compeltion_config

--- toucans_hub/db/models.py
+++ toucans_hub/db/models.py
@@ -14,7 +14,7 @@ class PromptFunction(SQLModel, table=True):
     id: int = Field(primary_key=True)
     name: str = Field(index=True)
     hash_id: str = Field(index=True)
-    chat_api_config: dict = Field(default={}, sa_column=Column(JSON))
+    completion_config: dict = Field(default={}, sa_column=Column(JSON))
     created_at: Optional[datetime] = Field(default_factory=datetime.utcnow)
     updated_at: Optional[datetime] = Field(
         default_factory=datetime.utcnow, sa_column_kwargs={"onupdate": datetime.utcnow}
@@ -24,34 +24,4 @@ class PromptFunction(SQLModel, table=True):
 class CreatePromptFunction(BaseModel):
     name: str
     hash_id: str
-    chat_api_config: dict
-
-
-# ---------------------------------------------------------------------------- #
-#                                     Users                                    #
-# ---------------------------------------------------------------------------- #
-
-from fastapi_users import models
-from fastapi_users.db import SQLModelBaseUser
-from sqlmodel import Field, SQLModel
-
-
-class User(SQLModelBaseUser, table=True):
-    first_name: str = Field(nullable=False)
-    last_name: str = Field(nullable=False)
-
-
-class UserCreate(models.CreateUpdateDictModel):
-    first_name: str
-    last_name: str
-    email: str
-    password: str
-
-
-class UserUpdate(models.CreateUpdateDictModel):
-    first_name: Optional[str] = None
-    last_name: Optional[str] = None
-
-
-class UserDB(User, models.BaseUserDB):
-    pass
+    completion_config: dict
